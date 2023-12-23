6dc8631961112642563eff827540e58e2199ab9c
Thirumalai <thirumalaicse2020@gmail.com>
2023-12-23 10:24:06 +0530

    updating api key

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -10,6 +10,8 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getweather(city: string, units: string) {
+    // use your own api key here
+    // update your api key from openweathermap.org
     return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=e3adf84ca46b7ee196fe9e5ca6450c3b&units=' + units);
   }
 }
